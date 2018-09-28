f46bea0d0ae265b92b7c4d56dca8d2be6be1b59f
danru123 <www2545418153@qq.com>
2018-09-28 19:19:36 +0800

    wdr

--- app/components/common/foot.vue
+++ app/components/common/foot.vue
@@ -1,11 +1,9 @@
 <template>
-<div>
     <ul class="foot">
-        <li v-for='item of tab'>
-            <router-link :to='item.url'>{{item.title}}</router-link>
+        <li v-for='(item,i) of tab' >
+            <router-link :to='item.url' :class="{cur:item.title==$route.name}">{{item.title}}</router-link>
         </li>
     </ul>
-</div>
 </template>
 
 <script>
@@ -25,32 +23,39 @@ export default {
                     title:'投票',
                     url:'/vote'
                 }
-            ]
+            ],
+            num:0
         }
+    },
+    methods:{
+        
     }
 }
 </script>
 
-<style>
-    ul{
+<style scoped>
+    ul.foot{
         list-style: none;
-    }
-    ul li{
-        float: left;
-        width: calc(100% / 3);
-        text-align: center;
-        
-    }
-    ul li a{
-        text-decoration: none;
-        color: #666;
-    }
-    .foot{
         position: absolute;
         bottom: 0;
         width: 100%;
-        height: 50px;
-        line-height: 50px;
+        height: 2rem;
+        overflow: hidden;
+        line-height: 2rem;
         background: #f5f5f5;
+        z-index: 999;
+    }
+    ul.foot li{
+        float: left;
+        width: calc(100% / 3);
+        text-align: center;
+     }
+    ul.foot li a{
+        text-decoration: none;
+        color: #666;
+        font-size: 0.8rem;
+    }
+    ul.foot li a.cur{
+        color:#52caec;
     }
 </style>
--- app/components/index.vue
+++ app/components/index.vue
@@ -1,20 +1,170 @@
 <template>
-    <div class="box">
-        <h1>我是首页</h1>
+    <div class="box" @touchmove='move' @touchstart='start'>
+        <div class="top">
+
+        </div>
+        <div class="content">
+            <p class="time slideInLeft animated">2018</p>
+            <p class="title slideInRight animated">Hyundai Blue Prize</p>
+            <p class="con">助力中国青年策展人的多领域艺术实践平台</p>
+            <p class="line"></p>
+            <p class="rote">入围作品线上投票</p>
+            <p class="start">正式开始</p>
+        </div>
+        <div id="global_scroll">
+            <img src="../../assets/scroll.png" alt="">
+        </div>
     </div>
     
 </template>
 
 <script>
+// import $ from 'zepto'
 export default {
-
+    data(){
+        return {
+            startY:null,
+            distanceY:null
+        }
+    },
+    methods:{
+        move(e){
+            this.distanceY=e.touches[0].clientY-this.startY;
+            if(this.distanceY<0){
+               this.$router.push('/description') 
+            }
+        },
+        start(e){
+            this.startY=e.touches[0].clientY;
+        }
+    }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
     .box{
         width: 100%;
         height: 100%;
         background-color: #52caec;
+        padding-top: 8rem;
+        box-sizing: border-box;
+        overflow: hidden;
     }
+    #global_scroll{
+        width: 2.5rem;
+        height: 2.5rem;
+        position: absolute;
+        left:44%;
+        transform: translateX(-50%);
+        bottom: 4%;
+        animation: scroll 2s infinite;
+    }
+    @keyframes scroll {
+        0% {
+            transform: translateX(0) translateY(0px) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
+        }
+        50% {
+            transform: translateX(0) translateY(10px) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
+        }
+        100% {
+            transform: translateX(0px) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
+        }
+    }
+    #global_scroll img{
+        width: 100%;
+        height: 100%;
+    }
+    .content{
+        width: 66%;
+        margin: 0 auto;
+        color:#e4dcd3;
+    }
+    .content{
+        .time,.title,.con{
+            font-size: 0.9rem;
+        }
+        .con{
+            letter-spacing:0.15rem;
+            animation: bottomTnto4 0.8s linear forwards;
+            position: absolute;
+            opacity: 0;
+        }
+        .line{
+            width: 68%;
+            border:1px solid #e4dcd3;
+            margin-top: 0.5rem;
+            margin-bottom: 0.5rem;
+            animation: line3 0.8s linear forwards;
+            animation-delay: .2s;
+            position: absolute;
+            opacity: 0;
+        }
+        .rote{
+            font-size: 0.8rem;
+            letter-spacing:0.5rem;
+            animation: bottomTnto2 0.8s linear forwards;
+            animation-delay: .4s;
+            position: absolute;
+            opacity: 0;
+        }
+        .start{
+            font-size: 1rem;
+            letter-spacing:1.64rem;
+            font-weight:500;
+            font-family: '微软雅黑';
+            position: absolute;
+            animation: bottomTnto1 0.8s linear forwards;
+            animation-delay: .6s;
+            opacity: 0;
+        }
+        @keyframes bottomTnto1 {
+            from{
+                bottom:-2rem;
+                opacity: 0;
+            }
+            to{
+                bottom:12rem;
+                opacity: 1;
+            }
+        }
+        @keyframes bottomTnto2 {
+            from{
+                bottom:-2rem;opacity: 0;
+            }
+            to{
+                bottom:13.5rem;
+                opacity: 1;
+            }
+        }
+        @keyframes line3 {
+            from{
+                bottom:-2rem;opacity: 0;
+            }
+            to{
+                bottom:14.5rem;
+                opacity: 1;
+            }
+        }
+        @keyframes bottomTnto4 {
+            from{
+                bottom:-2rem;opacity: 0;
+            }
+            to{
+                bottom:15.5rem;
+                opacity: 1;
+            }
+        }
+    }
+    .top{
+        position: absolute;
+        top: -8rem;
+        height: 15rem;
+        width: 17rem;
+        margin: 0 auto;
+        border: 4px solid #95def3;
+        border-radius: 50%;
+        left: 50%;
+        transform: translate(-50%);
+    }
+    
 </style>
